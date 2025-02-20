2ea2169a683dca464a6ac5e2834045661ecdca15
aglaia <aglaia.hub@gmail.com>
2025-02-20 15:07:49 +0200

    service

--- src/main/java/java55/farm/auth/service/AuthServiceImpl.java
+++ src/main/java/java55/farm/auth/service/AuthServiceImpl.java
@@ -2,11 +2,14 @@ package java55.farm.auth.service;
 
 import java55.farm.auth.dao.AccountRepository;
 import java55.farm.auth.dto.*;
+import java55.farm.auth.dto.exception.AccountNotFoundException;
+import java55.farm.auth.model.Account;
 import java55.farm.auth.model.Farm;
 import java55.farm.auth.model.User;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -34,33 +37,45 @@ public class AuthServiceImpl implements AuthService{
         return modelMapper.map(user, UserDto.class);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public AccountDto getAccountDto(String name) {
-        return null;
+        //TODO PRINCIPAL
+        Account account = accountRepository.findById(name).orElseThrow(AccountNotFoundException::new);
+        return modelMapper.map(account, AccountDto.class);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public FarmDto getFarmInfo(String id) {
-        return null;
+        Farm farm = (Farm) accountRepository.findById(id).orElseThrow(AccountNotFoundException::new);
+        return modelMapper.map(farm, FarmDto.class);
+
     }
 
+    @Transactional(readOnly = true)
     @Override
     public UserDto getUserInfo(String id) {
-        return null;
+        User user = (User) accountRepository.findById(id).orElseThrow(AccountNotFoundException::new);
+        return modelMapper.map(user, UserDto.class);
     }
 
     @Override
     public FarmDto updateFarmInfo(String id, FarmRegisterDto farmRegisterDto) {
+        //TODO
         return null;
     }
 
     @Override
     public UserDto updateUserInfo(String id, UserRegisterDto userRegisterDto) {
+        //TODO
         return null;
     }
 
     @Override
     public AccountDto deleteAccount(String id) {
-        return null;
+        Account account = accountRepository.findById(id).orElseThrow(AccountNotFoundException::new);
+        accountRepository.delete(account);
+        return modelMapper.map(account, AccountDto.class);
     }
 }
