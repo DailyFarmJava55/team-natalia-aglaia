564f3ddb8438ae78e430f1a930df9780db04b4a2
Nataly14145 <zayac.nataly@gmail.com>
2025-03-10 19:39:11 +0200

    first commit

--- Daily_Farm/src/main/java/telran/java55/auth/dto/UserDto.java
+++ Daily_Farm/src/main/java/telran/java55/auth/dto/UserDto.java
@@ -26,7 +26,7 @@ public class UserDto {
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
